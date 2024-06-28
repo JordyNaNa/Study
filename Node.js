d8c0adbd7a34c1fd01e703ae82424e1b14b39698
JordyNaNa <jspark030611@gmail.com>
2024-06-28 16:45:25 +0900

    SSG Web Practice Server1

---
+++ Node.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+
+app.get('/board/list', (req, res)=>{
+
+})
+
+app.get('/board/write', (req, res)=>{
+
+})
+
+app.get('/board/view', (req, res)=>{
+
+})
+
+app.get('/board/update', (req, res)=>{
+
+})
+
+app.post('/board/update', (req, res)=>{
+
+})
+
+app.get('/board/delete', (req, res)=>{
+
+})
+
+app.listen(3000, () => {
+    console.log('server onload');
+})
